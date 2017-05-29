a32b0a53eac0ceead5c11b8905a72fe8d2113d99
Diego Escalante Urrelo <diegoe@gmail.com>
2017-05-28 23:16:00 -0500

    tests: Don't duplicate fake_response_from_file

--- pdl_scraper/test_pdfurl_spider.py
+++ pdl_scraper/test_pdfurl_spider.py
@@ -5,6 +5,7 @@ from scrapy.http import TextResponse, Request
 
 from pdl_scraper.spiders.pdfurl_spider import PdfUrlSpider
 
+from _test_utils import fake_response_from_file
 
 class TestPdfUrlSpider(unittest.TestCase):
 
@@ -40,30 +41,3 @@ class TestPdfUrlSpider(unittest.TestCase):
             response = fake_response_from_file(filename)
             result = self.spider.find_pdfurl(codigo, response)
             self.assertEqual(expected[i], result)
-
-
-def fake_response_from_file(filename, url=None):
-    """
-    Create a Scrapy fake HTTP response from a HTML file
-    @param file_name: The relative filename from the responses directory,
-                      but absolute paths are also accepted.
-    @param url: The URL of the response.
-    returns: A scrapy HTTP response which can be used for unittesting.
-
-    taken from http://stackoverflow.com/a/12741030/3605870
-    """
-    if not url:
-        url = 'http://www.example.com'
-
-    request = Request(url=url)
-    if not filename[0] == '/':
-        responses_dir = os.path.dirname(os.path.realpath(__file__))
-        file_path = os.path.join(responses_dir, 'test_spiders_data', filename)
-    else:
-        file_path = filename
-
-    file_content = open(file_path, 'r').read()
-
-    response = TextResponse(url=url, request=request, body=file_content)
-    response._encoding = 'latin-1'
-    return response
--- pdl_scraper/test_spiders.py
+++ pdl_scraper/test_spiders.py
@@ -6,14 +6,15 @@ from scrapy.http import TextResponse, Request
 
 from pdl_scraper.spiders.proyecto_spider import ProyectoSpider
 
+from _test_utils import fake_response_from_file
+
 
 class TestProyectoSpider(unittest.TestCase):
     def setUp(self):
         self.spider = ProyectoSpider()
 
     def test_parse_pdfurl(self):
-        filename = os.path.join('test_spiders_data', '02764.html')
-        results = self.spider.parse_item(fake_response_from_file(filename))
+        results = self.spider.parse_item(fake_response_from_file('02764.html'))
         print(results.meta['item'])
         item = results.meta['item']
         self.assertEqual(item['numero_proyecto'], u'02764/2013-CR')
@@ -33,30 +34,3 @@ class TestProyectoSpider(unittest.TestCase):
                         u'Comisión de Educación  Juventud y Deporte')
         self.assertEqual(item['titulo_de_ley'], u'LEY UNIVERSITARIA')
         self.assertEqual(item['numero_de_ley'], u'Ley Nº: 30220')
-
-
-def fake_response_from_file(filename, url=None):
-    """
-    Create a Scrapy fake HTTP response from a HTML file
-    @param file_name: The relative filename from the responses directory,
-                      but absolute paths are also accepted.
-    @param url: The URL of the response.
-    returns: A scrapy HTTP response which can be used for unittesting.
-
-    taken from http://stackoverflow.com/a/12741030/3605870
-    """
-    if not url:
-        url = 'http://www.example.com'
-
-    request = Request(url=url)
-    if not filename[0] == '/':
-        responses_dir = os.path.dirname(os.path.realpath(__file__))
-        file_path = os.path.join(responses_dir, filename)
-    else:
-        file_path = filename
-
-    file_content = open(file_path, 'r').read()
-
-    response = TextResponse(url=url, request=request, body=file_content)
-    response._encoding = 'latin-1'
-    return response
